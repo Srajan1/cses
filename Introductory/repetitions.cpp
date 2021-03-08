adcf94ae7f7ede996ddd5f5eb86e94d59656a8ee
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-03-08 10:21:54 +0530

    repetitions

---
+++ Introductory/repetitions.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define loop(i, n) for (ll i = 0; i < n; ++i)
+#define loopf(i, k, n) for (ll i = k; i <= n; ++i)
+#define loopr(i, k, n) for (ll i = k; i >= n; --i)
+#define test(cases) \
+    ll cases;       \
+    input(cases);   \
+    while (cases--)
+#define print(a) printf("%lld\n", a)
+#define input(a) scanf("%lld", &a)
+#define inarr(a, n)            \
+    for (ll i = 0; i < n; ++i) \
+    scanf("%lld", &a[i])
+#define assign(arr, x, n) loop(i, n) arr[i] = x
+int main()
+{
+    string s;
+    cin>>s;
+    ll maxs = 1, curr = 1;
+    loopf(i, 1, s.size()-1){
+        if(s[i] == s[i-1])
+            ++curr;
+        else curr = 1;
+        maxs = max(curr, maxs);
+    }
+    cout<<maxs;
+}
