c742f4221e4c586bafc3c34054afc158c9fabed5
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-03-08 09:09:00 +0530

    introductory problem 1

---
+++ Introductory/weirdAlgorithm.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define loop(i, n) for (ll i = 0; i < n; ++i)
+#define loopf(i, k, n) for (ll i = k; i <= n; ++i)
+#define loopr(i, k, n) for (ll i = k; i >= n; --i)
+#define test(cases) \
+    ll cases;       \
+    input(cases);   \
+    while (cases--)
+#define print(a) printf("%lld\n", a)
+#define input(a) scanf("%lld", &a)
+#define inarr(a, n)            \
+    for (ll i = 0; i < n; ++i) \
+    scanf("%lld", &a[i])
+#define assign(arr, x, n) loop(i, n) arr[i] = x
+int main()
+{
+    ll n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n!=1){
+        if(n%2)
+        n = n*3+1;
+        else n/=2;
+        cout<<n<<" ";
+    }
+}
