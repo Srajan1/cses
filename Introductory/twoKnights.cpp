4ad67a92c40d4861c5e4fa2cf148ca37b4f250e3
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-04-07 17:24:45 +0530

    https://cses.fi/problemset/task/1072/

---
+++ Introductory/twoKnights.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define loop(i, n) for (ll i = 0; i < n; ++i)
+#define loopf(i, k, n) for (ll i = k; i <= n; ++i)
+#define loopr(i, k, n) for (ll i = k; i >= n; --i)
+#define test(cases) \
+    ll cases;       \
+    input(cases);   \
+    while (cases--)
+#define print(a) printf("%lld\n", a)
+#define input(a) scanf("%lld", &a)
+#define inarr(a, n)            \
+    for (ll i = 0; i < n; ++i) \
+    scanf("%lld", &a[i])
+#define assign(arr, x, n) loop(i, n) arr[i] = x
+#define v(x) vector<x>
+#define p(x,y) pair<x, y>
+
+
+int main()
+{
+    ll n;
+    cin>>n;
+    loopf(i, 1, n)
+    {
+        if(i==1)
+            cout<<0<<"\n";
+        else if(i==2)
+            cout<<6<<"\n";
+        else
+        {
+            ll p=i*i;
+            ll j=i-2,k=p*(p-1)/2;
+            k-=8*j*(j+1)/2;
+            cout<<k<<"\n";
+        }
+        
+    }
+}
