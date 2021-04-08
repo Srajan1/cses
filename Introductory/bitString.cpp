950cc62fab157e43ba2d8e3180be5b17119a0d1e
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-04-08 12:48:28 +0530

    https://cses.fi/problemset/task/1617/

---
+++ Introductory/bitString.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define loop(i, n) for (ll i = 0; i < n; ++i)
+#define loopf(i, k, n) for (ll i = k; i <= n; ++i)
+#define loopr(i, k, n) for (ll i = k; i >= n; --i)
+#define test(cases) \
+    ll cases;       \
+    input(cases);   \
+    while (cases--)
+#define print(a) printf("%lld\n", a)
+#define input(a) scanf("%lld", &a)
+#define inarr(a, n)            \
+    for (ll i = 0; i < n; ++i) \
+    scanf("%lld", &a[i])
+#define assign(arr, x, n) loop(i, n) arr[i] = x
+#define v(x) vector<x>
+#define p(x,y) pair<x, y>
+
+
+int main()
+{
+    ll n;
+    cin>>n;
+    ll val = 1;
+    loopf(i, 1, n){
+        val = (val%mod * 2)%mod;
+    }
+    cout<<val;
+}
