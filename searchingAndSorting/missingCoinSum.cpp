6757028e2048126001330432eccda94676e8d03d
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-04-09 14:57:26 +0530

    https://cses.fi/problemset/task/2183

---
+++ searchingAndSorting/missingCoinSum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define loop(i, n) for (ll i = 0; i < n; ++i)
+#define loopf(i, k, n) for (ll i = k; i <= n; ++i)
+#define loopr(i, k, n) for (ll i = k; i >= n; --i)
+#define test(cases) \
+    ll cases;       \
+    input(cases);   \
+    while (cases--)
+#define print(a) printf("%lld\n", a)
+#define input(a) scanf("%lld", &a)
+#define inarr(a, n)            \
+    for (ll i = 0; i < n; ++i) \
+    scanf("%lld", &a[i])
+#define assign(arr, x, n) loop(i, n) arr[i] = x
+#define v(x) vector<x>
+#define p(x,y) pair<x, y>
+
+int main()
+{
+    ll n;
+    cin>>n;
+    v(ll) arr(n);
+    inarr(arr, n);
+    sort(arr.begin(), arr.end());
+    ll sum = 0;
+    loop(i, n){
+        if(sum+1 < arr[i])
+            break;
+        else sum += arr[i];
+    }
+    cout<<sum+1;
+}
